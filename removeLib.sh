e0efa746813f462c2b7bdf2fced9e9765fcf8e10
Fernando.LC <fer.lc.developer@gmail.com>
2022-09-04 00:22:57 -0500

    feat(structure): update project

--- removeLib.sh
+++ removeLib.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo "************FOREVER JAJAJAJAJA***************+"
+
+FILE="package.json"
+
+REMOVE_DEV_DEPENDENCIES="devDependencies"
+
+REMOVE_DEPENDENCIES="dependencies"
+
+ARRAY_REMOVE=( $REMOVE_DEPENDENCIES $REMOVE_DEV_DEPENDENCIES )
+
+for i in ${ARRAY_REMOVE[@]}; do
+    sed -i '/\'"$i"'/,/}/ d; /^$/d' $FILE
+done
+
+# ADD_DEPENDENCIE=' "dependencies": { }';
+
+#sed -i '$s/'"}"'/ '"$ADD_DEPENDENCIE"'\n }/' $FILE
