55b11b4d9d357de0aa202f98e0e754cd047866c6
Fernando.LC <fer.lc.developer@gmail.com>
2022-09-04 00:35:55 -0500

    feat(structure): update project

--- removeLib.sh
+++ removeLib.sh
@@ -11,7 +11,3 @@ ARRAY_REMOVE=( $REMOVE_DEPENDENCIES $REMOVE_DEV_DEPENDENCIES )
 for i in ${ARRAY_REMOVE[@]}; do
     sed -i '/\'"$i"'/,/}/ d; /^$/d' $FILE
 done
-
-# ADD_DEPENDENCIE=' "dependencies": { }';
-
-#sed -i '$s/'"}"'/ '"$ADD_DEPENDENCIE"'\n }/' $FILE
